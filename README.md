740cf9ca1d81ea0ff9dbe4a1ed2541cfc0f19e97
Adam Slinkman <adam.slinkman@gmail.com>
2022-04-19 10:40:00 -0400

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# tsCourseProjectA
+#typescript Drag-&-Drop Project
+
+A simple course project utilizing form inputs, 'state' management, and drag & drop features.
--- src/components/project-list.ts
+++ src/components/project-list.ts
@@ -1,9 +1,9 @@
-import { Component } from './base-component.js';
-import { Project, ProjectStatus } from '../models/project.js';
-import { DragTarget } from '../models/drag-drop.js';
-import { projectState } from '../state/project-state.js';
-import { ProjectItem } from './project-item.js';
-import { autobind } from '../decorators/autobind.js';
+import { Component } from './base-component';
+import { Project, ProjectStatus } from '../models/project';
+import { DragTarget } from '../models/drag-drop';
+import { projectState } from '../state/project-state';
+import { ProjectItem } from './project-item';
+import { autobind } from '../decorators/autobind';
 
 // ProjectList class
 export class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DragTarget {
